fd222859981a7f2a83c814bc539d08f0546a4a83
znsoft <zn@bk.ru>
2017-06-07 23:43:16 +1000

    firs commit

---
+++ tusa1/Assets/scripts/login.js
@@ -0,0 +1,15 @@
+﻿
+var CryptoJS = require("crypto-js");
+
+
+function calchash() {
+    login = document.getElementById("login").value;
+    password = document.getElementById("password").value;
+
+    console.log(CryptoJS.HmacSHA1("Message", "Key"));
+    timestamp = new Date().getTime();
+    hash = login + ":" + CryptoJS.SHA1(CryptoJS.MD5(password).toString() + timestamp) + ":" + timestamp;
+    return hash;
+
+
+}
